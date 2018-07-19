9befd82e4b4f6e718239b20e4354401b13eb8623
Doyin Olarewaju <youngenterpreneur11@gmail.com>
2018-07-19 12:13:51 +0100

    Added eslint and flow-typed for jest

--- src/App/tests/App.test.js
+++ src/App/tests/App.test.js
@@ -1,70 +1,68 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {App} from '../App';
-import {shallow} from 'enzyme'
-import InitialState from '../constants'
-import thunk from 'redux-thunk'
-import Pod from '../components/Pod'
+import {shallow} from 'enzyme';
+import InitialState from '../constants';
+import Pod from '../components/Pod';
 
-const prepareProps = (props)=>({ ...InitialState, ...props })
+const prepareProps = (props)=>({ ...InitialState, ...props });
 
 describe('Landing Page Component', ()=>{
   it('Disconnected component renders without crashing', () => {
-    let wrapper = shallow(<App />, {disableLifecycleMethods:true})
-    expect(wrapper.length).toEqual(1)
+    const wrapper = shallow(<App />, {disableLifecycleMethods: true});
+    expect(wrapper.length).toEqual(1);
   });
 
   describe('Connected component', () => {
     it('renders without crashing', ()=>{
-      const mockFetchAllPodcastFn = jest.fn()
-      let wrapper = shallow(<App fetchAllPodcasts={mockFetchAllPodcastFn}/>)
-      expect(wrapper.length).toEqual(1)
-    })
+      const mockFetchAllPodcastFn = jest.fn();
+      const wrapper = shallow(<App fetchAllPodcasts={mockFetchAllPodcastFn}/>);
+      expect(wrapper.length).toEqual(1);
+    });
 
     it('calls fetchAllPodcasts function when mounted', ()=>{
-      const mockFetchAllPodcastFn = jest.fn()
-      let wrapper = shallow(<App fetchAllPodcasts={mockFetchAllPodcastFn}/>)
-      expect(mockFetchAllPodcastFn).toHaveBeenCalled()
-    })
+      const mockFetchAllPodcastFn = jest.fn();
+      const wrapper = shallow(<App fetchAllPodcasts={mockFetchAllPodcastFn}/>);
+      expect(mockFetchAllPodcastFn).toHaveBeenCalled();
+    });
 
     describe('displays', () => {
-      const mockFetchAllPodcastFn = jest.fn()
+      const mockFetchAllPodcastFn = jest.fn();
       it('loading state', ()=>{
-        let wrapper = shallow(<App 
-                                {...prepareProps({fetching:true})}
+        const wrapper = shallow(<App
+                                {...prepareProps({fetching: true})}
                                 fetchAllPodcasts={mockFetchAllPodcastFn}
-                              />)
-        expect(wrapper.find('.loading').length).toBe(1)
-      })
+                              />);
+        expect(wrapper.find('.loading').length).toBe(1);
+      });
 
       describe('podcast list of', ()=>{
         it('empty podcasts', ()=>{
-          const props = {fetching:false}
-          let wrapper = shallow(<App 
+          const props = {fetching: false};
+          const wrapper = shallow(<App
                                   {...prepareProps(props)}
                                   fetchAllPodcasts={mockFetchAllPodcastFn}
-                                />)
-          expect(wrapper.find('.loading').length).toBe(0)
-          expect(wrapper.find('.empty-podcasts').length).toBe(1)          
-        })
+                                />);
+          expect(wrapper.find('.loading').length).toBe(0);
+          expect(wrapper.find('.empty-podcasts').length).toBe(1);
+        });
 
-        it('empty', ()=>{
+        it('podcasts that are passed to', ()=>{
           const props = {
-            fetching:false,
-            podcasts:[
+            fetching: false,
+            podcasts: [
               // note i can just pass id only here and it will still work
-              {id:1, name:'Some Podcast', description:'Some description'}
-            ]
-          }
-          let wrapper = shallow(<App {...prepareProps(props)}
+              {id: 1, name: 'Some Podcast', description: 'Some description'},
+            ],
+          };
+          const wrapper = shallow(<App {...prepareProps(props)}
                                   fetchAllPodcasts={mockFetchAllPodcastFn}
-                                />)
-          expect(wrapper.find('.loading').length).toBe(0)
-          expect(wrapper.find('.empty-podcasts').length).toBe(0)
-          expect(wrapper.find(Pod).length).toBe(1)
-        })
-      })
-    })
-    
-  })
-})
+                                />);
+          expect(wrapper.find('.loading').length).toBe(0);
+          expect(wrapper.find('.empty-podcasts').length).toBe(0);
+          expect(wrapper.find(Pod).length).toBe(1);
+        });
+      });
+    });
+
+  });
+});
